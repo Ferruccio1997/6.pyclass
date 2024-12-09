634f4faab1deab45ce5e96d1bed8807055f2f49a
ferruccio_ribeiro <ferruccioribeiro@outlook.com>
2024-12-09 13:15:53 -0300

    Task finalizada

---
+++ main.py
@@ -0,0 +1,39 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        string_list = list(s)
+        i = 0
+
+        while i < len(string_list) - 2:
+            if string_list[i] == string_list[i + 1] == string_list[i + 2]:
+                print(f"Item removed: {string_list[i]}")
+                string_list.pop(i)
+                print(string_list)
+            else:
+                i += 1
+
+        return "".join(string_list)
+
+
+ddx = 8
+dx = 5
+
+
+a = [
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    111111111111111111111111111111111111111111111111111111111,
+]
